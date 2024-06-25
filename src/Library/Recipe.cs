d5a3447d6ee9e9911568d3bac18551ea887af472
mapachexx <fernandezfelipemateo@gmail.com>
2024-06-25 14:53:30 -0300

    done

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -9,12 +9,17 @@ using System.Collections.Generic;
 
 namespace Full_GRASP_And_SOLID
 {
-    public class Recipe : IRecipeContent // Modificado por DIP
+    public class Recipe : IRecipeContent, TimerClient // Modificado por DIP
     {
         // Cambiado por OCP
         private IList<BaseStep> steps = new List<BaseStep>();
 
         public Product FinalProduct { get; set; }
+        private bool cooked = false;
+        public bool Cooked 
+        {
+            get {return cooked;}
+        }
 
         // Agregado por Creator
         public void AddStep(Product input, double quantity, Equipment equipment, int time)
@@ -62,5 +67,30 @@ namespace Full_GRASP_And_SOLID
 
             return result;
         }
+
+        public int GetCookTime()
+        {
+            int resultado = 0;
+            foreach(BaseStep step in this.steps) 
+            {
+                resultado += step.Time;
+            }
+            return resultado;
+        }
+
+        public void Cook()
+        {
+            CountdownTimer timer = new CountdownTimer();
+            timer.Register(this.GetCookTime(), this);
+            this.cooked = true;
+        }
+
+        public void TimeOut ()
+        
+        {
+            Console.WriteLine($"La receta de {this.FinalProduct.Description} está pronta.");
+        }
+
+
     }
 }
